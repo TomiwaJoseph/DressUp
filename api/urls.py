953804bf6d9117e3a4158987b77d905860d4ba46
Tomiwa Joseph <tomiwajoseph88@gmail.com>
2022-08-19 23:57:30 +0100

    added more urls for data fetching

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list-them/', views.getData),
-    path('users/', views.UserCreate.as_view()),
-    path('login/', views.LoginView.as_view())
+    path('dress/<slug:slug>/', views.get_single__dress),
+    path('dress/related/<slug:slug>/', views.get_related__dress),
+    path('get-highest-price/', views.get_highest__price),
+    path('get-hottest-dresses/', views.get_hottest__dresses),
+    path('get-all-dresses-or-category/<slug:slug>/',
+         views.get_all_dresses_or_category),
+    path('get-all-categories/', views.get_all__categories),
+    path('filter-category-price/', views.filter_category_price),
+    # path('get-hottest-dresses/', views.TestView.as_view()),
+    path('create-user/', views.UserCreate.as_view(), name='user_create'),
+    path('login/', views.LoginView.as_view(), name='user_login')
 ]
