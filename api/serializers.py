092f348b8fa4df9a6df4da5fd32354c753ed5522
Tomiwa Joseph <tomiwajoseph88@gmail.com>
2022-08-19 23:58:24 +0100

    added custom fields to DressSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,23 +2,34 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from users.models import CustomUser
 from rest_framework.authtoken.models import Token
-# from base.models import Dresses
+from base.models import Dress, Category
 
 
-class DressSerializer(serializers.Serializer):
+class DressSerializer(serializers.ModelSerializer):
     class Meta:
-        # model = Dresses
+        model = Dress
+        fields = ['id', 'name', 'price',
+                  'discount_price', 'slug', 'main_image', ]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
         fields = '__all__'
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['email', 'password']
+        fields = ['email', 'password', 'first_name', 'last_name']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = CustomUser(email=validated_data['email'])
+        user = CustomUser(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
         user.set_password(validated_data['password'])
         user.save()
         Token.objects.create(user=user)
