0b3b45099c2c570aedc8cd251ff21f11a532e8d5
Tomiwa Joseph <tomiwajoseph88@gmail.com>
2022-08-27 23:28:19 +0100

    changed index.js to be react 18 compliant

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(rootElement).render(<App />);
