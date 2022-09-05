fcc048013c69681c54df54526207894cc7ad38fe
Tomiwa Joseph <tomiwajoseph88@gmail.com>
2022-09-06 00:55:39 +0100

    changed data store from context api to react-dredux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,64 +14,48 @@ import SingleProduct from "./pages/SingleProduct";
 import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 // import CartIcon from "./components/CartIcon";
 import Footer from "./components/Footer";
-import SignUp from "./pages/SignUp";
-import Preloader from "./components/Preloader";
-// import DressUpState from "./context/DressUpState";
 import Checkout from "./pages/Checkout";
 import Dashboard from "./pages/Dashboard";
-import PrivateRoute from "./PrivateRoute";
-// import dressContext from "./context/dressup-context";
-import fetchUser from "./context/auth";
-import DressState from "./context/DressState";
-import dressReducer from "./context/dress-reducer";
+// import PrivateRoute from "./PrivateRoute";
+import fetchUser from "./redux/actions/auth";
+import { fetchCartCount } from "./redux/actions/fetchers";
+import store from "./redux/store/store";
+import { setLoginUser } from "./redux/actions/dressActions";
+import TrackOrder from "./pages/TrackOrder";
+import ErrorPage from "./pages/ErrorPage";
 
 const App = () => {
   const getUserUrl = "http://localhost:8000/api/auth/user/";
-  const [isAuthenticated, setIsAuthenticated] = useState(null);
 
   useEffect(() => {
     let data = fetchUser(getUserUrl, (status) => {
-      setIsAuthenticated(status);
+      store.dispatch(setLoginUser(status));
     });
+    fetchCartCount();
   }, []);
 
   return (
     <Router>
-      <DressState isAuth={isAuthenticated}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop/dress/:dressSlug" element={<SingleProduct />} />
-          <Route path="/shop" element={<Shop />} />
-          {/* <Route
-            path="/user/dashboard"
-            element={
-              <PrivateRoute auth={isAuthenticated}>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          /> */}
-          <Route path="/user/dashboard" element={<Dashboard />} />
-          {/* <Route
-            path="/shop/checkout"
-            element={
-              <PrivateRoute>
-                <Checkout />
-              </PrivateRoute>
-            }
-          /> */}
-          {/* <Route path="/shop/category/:slug" element={<Category />} /> */}
-          <Route path="/about" element={<About />} />
-          <Route path="/contact-us" element={<Contact />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
-        </Routes>
-        {/* <CartIcon /> */}
-        <Footer />
-      </DressState>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop/dress/:dressSlug" element={<SingleProduct />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/user/dashboard" element={<Dashboard />} />
+        <Route path="/shop/checkout" element={<Checkout />} />
+        <Route path="/user/track-order/:orderId" element={<TrackOrder />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      {/* <CartIcon /> */}
+      <Footer />
     </Router>
   );
 };
