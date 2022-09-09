94ff9f1a3470485f9accc97253b80500b37f1f62
Tomiwa Joseph <tomiwajoseph88@gmail.com>
2022-09-09 14:54:04 +0100

    implemented a search mechanism

---
+++ frontend/src/pages/Search.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import CategoryPlusPagination from "../components/CategoryPlusPagination";
+import Error from "../components/Error";
+import Features from "../components/Features";
+import Preloader from "../components/Preloader";
+import { searchDressByName } from "../redux/actions/fetchers";
+import "./search.css";
+
+const Search = () => {
+  const [doneLoading, setDoneLoading] = useState(false);
+  const storeContext = useSelector((state) => state.dress);
+  const {
+    fetchingData,
+    noInternet,
+    searchResults,
+    backendUrl,
+    isAuthenticated,
+  } = storeContext;
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    searchDressByName(e.target.name.value);
+    setDoneLoading(true);
+    e.target.reset();
+  };
+  const renderSearchData = () => {
+    if (!searchResults.length && doneLoading) {
+      return (
+        <div className="col-12 text-center">
+          <p className="no__dress">No dress match your search parameters</p>
+        </div>
+      );
+    } else {
+      return (
+        <>
+          <CategoryPlusPagination
+            categoryData={searchResults}
+            backendUrl={backendUrl}
+            isAuthenticated={isAuthenticated}
+          />
+          <Features />
+        </>
+      );
+    }
+  };
+
+  if (fetchingData) {
+    return <Preloader />;
+  }
+
+  if (noInternet) {
+    return <Error />;
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title__caption">Search</h1>
+      <hr className="underline" />
+      <form onSubmit={handleSubmitForm} className="search-form">
+        <input type="text" name="name" className="form-control" required />
+      </form>
+      {renderSearchData()}
+    </div>
+  );
+};
+
+export default Search;
