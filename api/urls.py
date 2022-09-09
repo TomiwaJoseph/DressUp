03724b52e4f1f6c4c1a8bae8f343fc3cb4e5c272
Tomiwa Joseph <tomiwajoseph88@gmail.com>
2022-09-09 14:20:52 +0100

    added more api endpoints

--- api/urls.py
+++ api/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
          views.get_all_dresses_or_category),
     path('get-all-categories/', views.get_all_categories),
     path('filter-category-price/', views.filter_category_price),
-    path('user-order-history/', views.get_order_history),
     path('save-paylater-details/', views.save_paylater_details),
     path('add-to-wishlist/', views.add_to_wishlist),
     path('add-to-cart/', views.add_to_cart),
@@ -23,13 +22,17 @@ urlpatterns = [
     path('fetch-wishlist-dresses/', views.get_wishlist_dresses),
     path('delete-wishlist-dress/', views.delete_wishlist_dress),
     path('get-user-orders/', views.get_user_orders),
+    path('get-specific-order/', views.get_specific_order),
+    path('request-refund/', views.request_refund),
+    path('add-to-newsletter/', views.add_to_newsletter),
+    path('search-dress/', views.search_dress),
     # AUTHENTICATION URLS
     path('auth/register/', views.RegisterView.as_view(), name='register'),
     path('auth/login/', views.LoginView.as_view(), name='login'),
+    path('login-demo-user/', views.login_demo_user, name='demo-login'),
     path('auth/logout/', views.logout),
-    # path('auth/user/', views.UserAPI.as_view(), name='get_user'),
     path('auth/user/', views.fetch_user, name='fetch_user'),
     # STRIPE URL
-    path('stripe-payment/', views.stripe_payment, name='stripe-payment'),
+    # path('stripe-payment/', views.stripe_payment, name='stripe-payment'),
     path('save-stripe-info/', views.save_stripe_info, name='save-stripe-info'),
 ]
