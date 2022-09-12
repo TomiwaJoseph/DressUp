e8daf900bd3e05b14e997350c3d87bce88792693
Tomiwa Joseph <tomiwajoseph88@gmail.com>
2022-09-12 22:41:10 +0100

    created views for django to handle from front-end

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def get_dress_or_order(request, slug):
+    return render(request, 'index.html')
--- dressUp/urls.py
+++ dressUp/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', include('base.urls'))
 ]
 
 if settings.DEBUG:
